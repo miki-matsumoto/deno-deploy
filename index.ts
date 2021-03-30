7088d8cbb5218ee54e9de4d5de5b6d5c9d553c81
miki-matsumoto <mikimatsumoto829@gmail.com>
2021-03-31 00:39:15 +0900

    fetch

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
-  event.respondWith(response);
+addEventListener("fetch", async (event) => {
+  let response = await fetch('https://staging.rolle.io/index.js')
+  const headers = { "cache-control": "public, max-age=14400", 'content-type': 'text/javascript' }
+  response = new Response(response.body, { ...response, headers })
+
+  return response;
 });
